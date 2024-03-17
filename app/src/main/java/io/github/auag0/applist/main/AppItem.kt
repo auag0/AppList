27f8ec291e610d403fece336b3ea7f6fe7306022
anago <outpe1119@gmail.com>
2024-03-17 17:03:19 +0900

    implemented Parcelable in AppItem

--- app/src/main/java/io/github/auag0/applist/main/AppItem.kt
+++ app/src/main/java/io/github/auag0/applist/main/AppItem.kt
@@ -1,6 +1,8 @@
 package io.github.auag0.applist.main
 
 import android.content.pm.PackageInfo
+import android.os.Parcel
+import android.os.Parcelable
 
 data class AppItem(
     val packageInfo: PackageInfo,
@@ -8,4 +10,34 @@ data class AppItem(
     val packageName: String,
     val isSystem: Boolean,
     val lastUpdateTime: Long
-)
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readParcelable(PackageInfo::class.java.classLoader)!!,
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readByte() != 0.toByte(),
+        parcel.readLong()
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeParcelable(packageInfo, flags)
+        parcel.writeString(name.toString())
+        parcel.writeString(packageName)
+        parcel.writeByte(if (isSystem) 1 else 0)
+        parcel.writeLong(lastUpdateTime)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<AppItem> {
+        override fun createFromParcel(parcel: Parcel): AppItem {
+            return AppItem(parcel)
+        }
+
+        override fun newArray(size: Int): Array<AppItem?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
