9771ce2cce287c946ae00db07e204f6ef718fcc9
anago <outpe1119@gmail.com>
2024-03-17 17:07:31 +0900

    moved to view binding

--- app/src/main/java/io/github/auag0/applist/main/MainActivity.kt
+++ app/src/main/java/io/github/auag0/applist/main/MainActivity.kt
@@ -14,10 +14,7 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.google.android.material.color.MaterialColors
-import com.google.android.material.progressindicator.LinearProgressIndicator
 import io.github.auag0.applist.R
 import io.github.auag0.applist.utils.AppPrefsManager.AppFilter.AllApps
 import io.github.auag0.applist.utils.AppPrefsManager.AppFilter.SystemApps
@@ -35,30 +32,30 @@ import io.github.auag0.applist.core.utils.AppPrefsManager.AppSort.ByLastUpdateTi
 import io.github.auag0.applist.core.utils.AppPrefsManager.AppSort.ByName
 import io.github.auag0.applist.core.utils.AppPrefsManager.appFilter
 import io.github.auag0.applist.core.utils.AppPrefsManager.appSort
-import io.github.auag0.applist.core.views.SearchBar
+import io.github.auag0.applist.databinding.ActivityMainBinding
 import kotlinx.coroutines.launch
 import me.zhanghai.android.fastscroll.FastScrollerBuilder
 
 class MainActivity : BaseActivity() {
     private val viewModel: MainViewModel by viewModels()
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val searchBar: SearchBar = findViewById(R.id.searchBar)
-        setSupportActionBar(searchBar)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.searchBar)
 
-        val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
         val adapter = AppAdapter(viewModel)
-        recyclerView.adapter = adapter
-        recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = adapter
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
 
-        FastScrollerBuilder(recyclerView)
+        FastScrollerBuilder(binding.recyclerView)
             .useMd2Style()
             .setPopupTextProvider(adapter)
             .build()
 
-        searchBar.addTextChangedListener(object : TextWatcher {
+        binding.searchBar.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
 
             override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
@@ -69,19 +66,17 @@ class MainActivity : BaseActivity() {
             override fun afterTextChanged(s: Editable?) {}
         })
 
-        val progressIndicator: LinearProgressIndicator = findViewById(R.id.linearProgressIndicator)
-        val swipeRefreshLayout: SwipeRefreshLayout = findViewById(R.id.swipeRefreshLayout)
         val progressBgColor = MaterialColors.getColor(
-            swipeRefreshLayout,
+            binding.swipeRefreshLayout,
             com.google.android.material.R.attr.colorPrimaryContainer
         )
-        swipeRefreshLayout.setProgressBackgroundColorSchemeColor(progressBgColor)
+        binding.swipeRefreshLayout.setProgressBackgroundColorSchemeColor(progressBgColor)
         val progressColor = MaterialColors.getColor(
-            swipeRefreshLayout,
+            binding.swipeRefreshLayout,
             com.google.android.material.R.attr.colorPrimary
         )
-        swipeRefreshLayout.setColorSchemeColors(progressColor)
-        swipeRefreshLayout.setOnRefreshListener {
+        binding.swipeRefreshLayout.setColorSchemeColors(progressColor)
+        binding.swipeRefreshLayout.setOnRefreshListener {
             viewModel.loadAppList()
         }
 
@@ -90,7 +85,7 @@ class MainActivity : BaseActivity() {
                 launch {
                     viewModel.filteredAppList.collect { appList ->
                         adapter.submitList(appList) {
-                            recyclerView.scrollToPosition(0)
+                            binding.recyclerView.scrollToPosition(0)
                         }
                     }
                 }
@@ -98,13 +93,16 @@ class MainActivity : BaseActivity() {
                 launch {
                     viewModel.progress.collect { progress ->
                         if (progress == null) {
-                            progressIndicator.visibility = View.GONE
-                            swipeRefreshLayout.isRefreshing = false
+                            binding.linearProgressIndicator.visibility = View.GONE
+                            binding.swipeRefreshLayout.isRefreshing = false
                         } else {
-                            progressIndicator.visibility = View.VISIBLE
-                            progressIndicator.max = progress.max
-                            progressIndicator.setProgressCompat(progress.current, true)
-                            swipeRefreshLayout.isRefreshing = true
+                            binding.linearProgressIndicator.visibility = View.VISIBLE
+                            binding.linearProgressIndicator.max = progress.max
+                            binding.linearProgressIndicator.setProgressCompat(
+                                progress.current,
+                                true
+                            )
+                            binding.swipeRefreshLayout.isRefreshing = true
                         }
                     }
                 }
